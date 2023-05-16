0e826023382484d63691c78b50c20027eac5530d
Kamal Hossain <k.hossain@rrive.com>
2023-05-16 23:31:35 +0200

    Add project files.

---
+++ ConsoleAppIPValidation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+//while(!Console.KeyAvailable) //(Console.ReadKey(true).Key != ConsoleKey.X)
+do 
+{
+    // Type your ip and press enter
+    Console.WriteLine("enter Ip");
+    // Create a string variable and get user input from the keyboard and store it in the variable
+    string ip = Console.ReadLine();
+    //Initialize IP address
+    //IPAddress ipaddrss;
+    //Regular expression of IP address
+    Regex regip = new Regex(@"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b");
+    //find match between regular expression and enterd value
+    MatchCollection result = regip.Matches(ip);
+    if (result.Count == 0)
+    {
+        Console.WriteLine("Invalid");
+    }
+    else
+    {
+        Console.WriteLine("Valid");
+    }
+}
+while (Console.ReadKey().Key != ConsoleKey.X);
+
